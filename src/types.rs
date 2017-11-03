131c7c97b4cc70fb45c94b12978587e23cdffeed
Marcus Ball <marcus@marcusball.me>
2017-11-02 23:49:17 -0400

    remove the dumb separate function for serializing

--- src/types.rs
+++ src/types.rs
@@ -6,16 +6,8 @@ pub struct Base64Bytes<T: ?Sized + AsRef<[u8]>>(pub T);
 
 impl<T: ?Sized + AsRef<[u8]>> Serialize for Base64Bytes<T> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
-        serialize_base64(&self.0, serializer)
+        base64::encode(&self.0).serialize(serializer)
     }
-}
-
-fn serialize_base64<'a, T, S>(field: &T, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-    T: ?Sized + AsRef<[u8]>,
-{
-    base64::encode(field).serialize(serializer)
 }
 
 
