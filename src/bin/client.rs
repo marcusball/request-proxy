26d94d643060454023407a8e00926326c594902d
Marcus Ball <marcus@marcusball.me>
2017-11-06 14:08:39 -0500

    parse destination host Url

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,10 +1,11 @@
-extern crate request_proxy;
 extern crate base64;
+extern crate dotenv;
+extern crate hyper;
+extern crate request_proxy;
+extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
-extern crate reqwest;
-extern crate dotenv;
-extern crate hyper; 
+extern crate url;
 
 use request_proxy::types::*;
 
@@ -13,8 +14,9 @@ use std::io::Read;
 use std::str::FromStr;
 use dotenv::dotenv;
 
-use hyper::{Method, Uri, HttpVersion};
-use reqwest::header::{Headers, Raw};
+use hyper::{HttpVersion, Method, Uri};
+use reqwest::header::{Headers, Host, Raw};
+use url::Url;
 
 fn main() {
     dotenv().ok();
@@ -23,13 +25,17 @@ fn main() {
     let server = env::var("REQUEST_PROXY_SERVER").expect("Missing REQUEST_PROXY_SERVER variable!");
 
     // Hostname or IP of the server to which to send proxied requests
-    let host = env::var("REQUEST_PROXY_HOST").expect("Missing REQUEST_PROXY_HOST destination variable!");
+    let host = Url::from_str(&env::var("REQUEST_PROXY_HOST")
+        .expect("Missing REQUEST_PROXY_HOST destination variable!"))
+        .expect("Failed to parse destination url!");
 
     loop {
         let mut response = reqwest::get(&server).unwrap();
 
         let mut content = String::new();
-        response.read_to_string(&mut content).expect("Failed to read response body!");
+        response
+            .read_to_string(&mut content)
+            .expect("Failed to read response body!");
 
         if content.eq("NONE") {
             thread::sleep(time::Duration::from_millis(500));
@@ -41,8 +47,9 @@ fn main() {
         let headers = build_headers(&request);
         let body = String::from_utf8(request.body.0).unwrap();
 
-        println!("{} {} {}\n{}\n{}", 
-            Method::from_str(request.method).unwrap(), 
+        println!(
+            "{} {} {}\n{}\n{}",
+            Method::from_str(request.method).unwrap(),
             Uri::from_str(request.uri).unwrap(),
             HttpVersion::from_str(&request.version).unwrap(),
             headers,
@@ -53,9 +60,12 @@ fn main() {
 
 /// Builds a Headers object from the raw header values in the ProxiedRequest
 fn build_headers(request: &ProxiedRequest) -> Headers {
-    request.headers.iter().fold(Headers::new(), |mut headers, &(k, ref v)| {
-        let value_bytes: &[u8] = v.0.as_ref();
-        headers.append_raw(String::from(k), Raw::from(value_bytes));
-        headers
-    })
+    request
+        .headers
+        .iter()
+        .fold(Headers::new(), |mut headers, &(k, ref v)| {
+            let value_bytes: &[u8] = v.0.as_ref();
+            headers.append_raw(String::from(k), Raw::from(value_bytes));
+            headers
+        })
 }
